02573045e054b1f2d4a43a2a0d0b721f6c8549b5
Harish <harishtella@gmail.com>
2011-05-18 15:51:14 -0500

    complete EX 5.14

--- ch5-regsim.scm
+++ ch5-regsim.scm
@@ -423,3 +423,46 @@
       false))
 
 '(REGISTER SIMULATOR LOADED)
+
+
+
+
+
+; EX 5.14 
+; number of stack pushes and max stack size is 
+; (n - 1) / 2 
+(define fact-machine
+  (make-machine
+	(list (list 'read read) 
+		  (list 'print display)
+		  (list '= =) (list '- -) (list '* *))
+	'(controller
+	   start
+	   (perform (op initialize-stack))
+	   (assign n (op read))
+	   (assign continue (label fact-done))     ; set up final return address
+	   fact-loop
+	   (test (op =) (reg n) (const 1))
+	   (branch (label base-case))
+	   ;; Set up for the recursive call by saving n and continue.
+	   ;; Set up continue so that the computation will continue
+	   ;; at after-fact when the subroutine returns.
+	   (save continue)
+	   (save n)
+	   (assign n (op -) (reg n) (const 1))
+	   (assign continue (label after-fact))
+	   (goto (label fact-loop))
+	   after-fact
+	   (restore n)
+	   (restore continue)
+	   (assign val (op *) (reg n) (reg val))   ; val now contains n(n-1)!
+	   (goto (reg continue))                   ; return to caller
+	   base-case
+	   (assign val (const 1))                  ; base case: 1!=1
+	   (goto (reg continue))                   ; return to caller
+	   fact-done
+	   (perform (op print) (reg val))
+	   (perform (op print-stack-statistics))
+	   (perform (op print) (const "\n"))
+	   (goto (label start)))))
+
